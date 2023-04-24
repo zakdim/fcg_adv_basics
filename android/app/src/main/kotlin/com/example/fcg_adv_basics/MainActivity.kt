26129a28889d01c0deeece60241af986e8dc27b8
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2023-04-23 22:23:01 -0400

    s03-52 Init app

---
+++ android/app/src/main/kotlin/com/example/fcg_adv_basics/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fcg_adv_basics
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) => runApp(const MyApp());
+
+class MyApp extends MaterialApp {
+  const MyApp({super.key});
+}
