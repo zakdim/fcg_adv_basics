3390222fc5acd0b41001cab62bfeaa7e17ed6d33
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2023-04-25 22:07:11 -0400

    s03-53 Challenge solution 1/2 - creating a widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:fcg_adv_basics/gradient_container.dart';
+import 'package:fcg_adv_basics/start_screen.dart';
 
 void main(List<String> args) => runApp(const MyApp());
 
@@ -10,13 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Advanced Basics Demo',
-
       home: Scaffold(
-        body: GradientContainer(
-          Color.fromARGB(255, 33, 5, 109),
-          Color.fromARGB(255, 68, 21, 149),
-        ),
+        body: StartScreen(),
       ),
     );
   }
